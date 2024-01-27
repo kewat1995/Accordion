4f74d133d070c33efecaf568c32f42f67c36e74b
kewat1995 <surendra121030@gmail.com>
2024-01-27 11:18:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./App.css";
+import data from "./data/data";
+
+function App() {
+  const [Selection, setSelection] = useState(null);
+  const [enablemultiselection, setMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+  const handelSingleSelection = (getcurrentId) => {
+    setSelection(getcurrentId === Selection ? null : getcurrentId);
+  };
+  const handelMultiselection = (getcurrentId) => {
+    let copyMultiple = [...multiple];
+    const findexIndex = copyMultiple.indexOf(getcurrentId);
+    if (findexIndex == -1) {
+      copyMultiple.push(getcurrentId);
+    } else {
+      copyMultiple.splice(findexIndex, 1);
+    }
+
+    setMultiple(copyMultiple);
+  };
+  // console.log(Selection, multiple);
+  return (
+    <div className="container">
+      <button onClick={() => setMultiSelection(!enablemultiselection)}>
+        Multiple Selection Enabled
+      </button>
+      <div className="content">
+        {data.map((item) => (
+          <div className="items" key={item.id}>
+            <div
+              className="title"
+              onClick={
+                enablemultiselection
+                  ? () => handelMultiselection(item.id)
+                  : () => handelSingleSelection(item.id)
+              }
+            >
+              <h2>{item.question}</h2>
+              <span>+</span>
+            </div>
+            {enablemultiselection ? (
+              multiple.indexOf(item.id) >= 0 && (
+                <div className="answer">{item.answer}</div>
+              )
+            ) : Selection === item.id ? (
+              <div className="answer">{item.answer}</div>
+            ) : null}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
